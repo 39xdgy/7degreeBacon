33c68b83bd4482de46d52bd40823e4ad747b96bc
39xdgy <35781890+39xdgy@users.noreply.github.com>
2018-03-07 07:39:41 -0600

    Add files via upload
    working

--- test.py
+++ test.py
@@ -14,17 +14,53 @@ def search(list):
     else:
         print("Did not find the name in the list")
 
+def find(list):
+    Starting = input("Who you want to start to?\n")
+    Ending = input("Who you want to end with?\n")
+    sublist = list[Starting]
+    count = 1
+    while(True):
+        if (Ending in sublist):
+            return count
+        else:
+            length = len(sublist)
+            for i in range(length):
+                getpeople = sublist[i]
+                getfriend = list[getpeople]
+                sublist.extend(getfriend)
+            count += 1
+
 
 list = {}
 print("Welcome to use the program")
 print("Please type in what do you want to do")
-user_input = input("add, search, change, delete, quit\n")
+user_input = input("add, test, search, find, quit\n")
 while(user_input != "quit"):
     if(user_input == "add"):
         add(list)
     if(user_input == "search"):
         search(list)
-    user_input = input("add, search, change, delete, quit\n")
+
+    if(user_input == "test"):
+        list["Alex"] = ["Anthony"]
+        list["Anthony"] = ["Alex", "Paul", "Karen", "Jason"]
+        list["Paul"] = ["Anthony", "Karen"]
+        list["Karen"] = ["Paul", "Anthony", "Salem", "Joe", "Sam"]
+        list["Salem"] = ["Karen", "Jason", "Joe", "Nigel"]
+        list["Jason"] = ["Miku", "Luka", "Jill", "Anthony", "Salem"]
+        list["Miku"] = ["Jason", "Luka"]
+        list["Luka"] = ["Jason", "Miku"]
+        list["Jill"] = ["Jason", "Joe"]
+        list["Joe"] = ["Jill", "Salem", "Joy", "Karen"]
+        list["Joy"] = ["Joe"]
+        list["Sam"] = ["Karen", "Tucker", "Dan", "Nigel"]
+        list["Nigel"] = ["Salem", "Sam"]
+        list["Tucker"] = ["Sam"]
+        list["Dan"] = ["Sam"]
+    if(user_input == "find"):
+        Solution = find(list)
+        print(Solution)
+    user_input = input("add, test, search, find, quit\n")
 
 
 
